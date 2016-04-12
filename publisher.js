3a6c891cc67523285df4711f9e94f6c0af7f202c
Sergii_Shust <sergii_shust@gmail.com>
2016-04-13 00:50:43 +0300

    SH-17 [SPIKE] MQTT + MQTTBroker investigatinon and building working example.

---
+++ publisher.js
@@ -0,0 +1,22 @@
+var mqtt = require('mqtt'), url = require('url');
+// Parse
+var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'CLOUD_INSTANCE');
+var auth = (mqtt_url.auth || 'USERNAME:PASSWORD').split(':');
+
+// Create a client connection
+var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
+    username: auth[0],
+    password: auth[1]
+});
+
+client.on('connect', function() { // When connected
+
+    setInterval(publishMessage, 4000)
+
+    function publishMessage(){
+        // publish a message to a topic
+        client.publish('hello/world', Date(), function() {
+            console.log("Message is published");
+        });
+    }
+});
