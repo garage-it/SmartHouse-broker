7eeb5c20959558b630f4f9e3ed5e791e37b3b1de
Sergii_Shust <sergii_shust@gmail.com>
2016-04-13 23:59:54 +0300

    SH-17 [SPIKE] added local broker

---
+++ Local/server.js
@@ -0,0 +1,32 @@
+var mosca = require('mosca');
+
+var pubsubsettings = {
+    //using ascoltatore
+    type: 'mongo', // use mongo DB to store published data
+    url: 'mongodb://localhost:27017/mqtt', //mqtt - is a DB in mongo
+    pubsubCollection: 'ascoltatori', //collection in MongoDB
+    mongo: {}
+};
+
+var settings = {
+    port: 1883, // port for mqtt client e.g. 'mqtt://localhost:1883'
+    backend: pubsubsettings
+};
+
+var server = new mosca.Server(settings);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+    console.log('Published', packet.payload);
+});
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+    console.log('Mosca server is up and running');
+}
---
+++ Local/subscribe.js
@@ -0,0 +1,23 @@
+var mqtt = require('mqtt'), url = require('url');
+// Parse
+var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'CLOUD');
+var auth = (mqtt_url.auth || 'USERNAME:PASSWORD').split(':');
+
+// Create a client connection
+var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
+    username: auth[0],
+    password: auth[1]
+});
+
+client.on('connect', function() { // When connected
+
+    // subscribe to a topic
+    client.subscribe('hello/world', function() {
+        // when a message arrives, do something with it
+        client.on('message', function(topic, message, packet) {
+            console.log("Received '" + message + "' on '" + topic + "'");
+            console.log("Packet " + packet);
+            console.log("***********************************");
+        });
+    });
+});
