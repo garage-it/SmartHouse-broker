7d991a082d1ceba5f806273e91efd00e87b287a1
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2016-04-23 02:33:51 +0300

    extended published message

--- Local/publisher.js
+++ Local/publisher.js
@@ -3,6 +3,7 @@ var mqtt = require('mqtt'), url = require('url');
 var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
 var auth = (mqtt_url.auth || 'USERNAME:PASSWORD').split(':');
 
+
 // Create a client connection
 var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
     username: auth[0],
@@ -11,12 +12,26 @@ var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
 
 client.on('connect', function() { // When connected
 
-    setInterval(publishMessage, 4000)
+    var devices = [ 'iddqd', 'other', 'primary' ];
+    var index = 0;
+
+    setInterval(publishMessage, 500);
 
     function publishMessage(){
         // publish a message to a topic
-        client.publish('hello/world', Date(), function() {
-            console.log("Message is published");
+        var message = JSON.stringify({
+            device: devices[index],
+            time: +new Date(),
+            value: 'mqtt eventio #' + index
         });
+
+        client.publish('event', message, function() {
+            console.log(message, ' <= message is published');
+        });
+
+        index++;
+        if (index >= devices.length){
+            index = 0;
+        }
     }
 });
