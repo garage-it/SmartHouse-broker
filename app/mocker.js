d51380a33733fb35a75ffb743ed825fa756587d8
Oleksandr_Tsaruk <alex.zogich@yandex.ru>
2016-12-24 15:22:44 +0200

    fix(TG-445): extend broker with socket device

--- app/mocker.js
+++ app/mocker.js
@@ -2,19 +2,23 @@
 
 const client = require('./common').connect();
 
-client.on('connect', ()=>{
-    const devices = ['temperature', 'humidity', 'distance', 'servo'];
+client.on('connect', ()=> {
+    const devices = ['temperature', 'humidity', 'distance', 'servo', 'socket'];
     let index = 0;
 
     (function publishMessage() {
-        const message = (Math.random() * 100).toFixed();
+        let message = (Math.random() * 100).toFixed();
         const topic = `/smart-home/out/${devices[index]}`;
+
+        if (index === 4) {
+            message = Math.random() > 0.5 ? 'ON' : 'OFF';
+        }
 
         client.publish(topic, message);
 
         index++;
 
-        if (index >= devices.length){
+        if (index >= devices.length) {
             index = 0;
         }
         setTimeout(publishMessage, 1000);
